2345a61d659bf1f297d37eccc4a12854e815e906
Timothy Wood <codearachnid@gmail.com>
2015-12-20 15:04:37 -0500

    update for full marking

--- Measurement Markings.js
+++ Measurement Markings.js
@@ -4,10 +4,10 @@
  * dependencies: none
  */
     var properties = [
-      {id: "Machine Size", type: "list", value: "500", options: [["300", "500mm"], ["800", "1000mm"]]}
+      {id: "Machine Size", type: "list", value: "500", options: [["100", "500mm"], ["800", "1000mm"]]}
+      // {id: "Depth of Cut", type: "range",value: 100, min: 3, max: 500, step: 1
     ];
 
-    var cutDepth = "rgb(100,100,100)";
     var markerLength = 5;
 
     var executor = function(args, success, failure) {
@@ -17,12 +17,12 @@
       var width = params["Machine Size"];
       var left = 0;
       var bottom = 0;
-      var depth = "rgb(215,215,215)";
-      cutDepth = depth;
+      var cutDepth = "rgb(215,215,215)";
+      var blockSize = 50;
       
       var svg = ['<?xml version="1.0" standalone="no"?>',
         '<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="' + width + dimension + '" height="' + height + dimension + '" viewBox="' + left + ' ' + bottom + ' ' + width + ' ' + height + '">',
-        '<path d="' + paths() + '" style="stroke:' + cutDepth + '; fill:none;" />',
+        '<path d="' + paths(width,height,blockSize) + '" style="stroke:' + cutDepth + '; fill:none;" />',
         '</svg>'
         ].join("");
 
@@ -30,24 +30,30 @@
     };
 
 
-function paths(){
-  return markingBlock(0,0,50);
-  // return 'M0,0, L100,100';
+function paths(width,height,size){
+  var blocks = [];
+  for(var i=0;i<(height/size);i++){
+    for(var ii=0;ii<(width/size);ii++){
+       blocks.push( markingBlock(ii*size,i*size,size) );
+    }
+  }
+  return blocks.join("");
 }
 
 function markingBlock(x,y,size){
   
   var block = [
-    'M' + [x,y].join(','),
-    // rect( x, y, size, size ),
-    // edgelines
-    // edgeLine( 'left', x, y+size, markerLength ),
-    // edgeLine( 'right', x+size, y+size, markerLength ),
-    // edgeLine( 'top', y, x+size, markerLength ),
-    // edgeLine( 'bottom', y+size, x+size, markerLength ),
+    // moveTO( x, y ),
+    // bounding box
+    rect( x, y, size, size ),
+    // edge lines
+    edgeLine( 'top', y, x+size, markerLength ),
+    edgeLine( 'left', x, y+size, markerLength ),
+    edgeLine( 'right', x+size, y+size, markerLength ),
+    edgeLine( 'bottom', y+size, x+size, markerLength ),
     // crosshair
-    line( x+(size/2), x+(size/2), y+(size/2)-markerLength, y+(size/2)+markerLength ),
-    line( x+(size/2)-markerLength, x+(size/2)+markerLength, y+(size/2), y+(size/2) ),
+    line( x+(size/2), y+(size/2)-markerLength, x+(size/2), y+(size/2)+markerLength ),
+    line( x+(size/2)-markerLength, y+(size/2), x+(size/2)+markerLength, y+(size/2) ),
     ];
   return block.join(' '); 
 }
@@ -56,33 +62,35 @@ function edgeLine( side, start, distance, increment ){
   var markers = [];
   for(var i=0;i<(distance/increment);i++){
     if( side == 'top' ){
-      markers.push( line( i*increment, i*increment, start, start + markerLength ) );
+      markers.push( line( i*increment, start, i*increment, start + markerLength ) );
     } else if ( side == 'bottom' ){
-      markers.push( line( i*increment, i*increment, start, start - markerLength ) );
+      markers.push( line( i*increment, start, i*increment, start - markerLength ) );
     } else if ( side == 'left' ){
-      markers.push( line( start, start + markerLength, i*increment, i*increment ) );
+      markers.push( line( start, i*increment, start + markerLength, i*increment ) );
     } else if ( side == 'right' ){
-      markers.push( line( start, start - markerLength, i*increment, i*increment ) );
+      // console.log(start, i*increment, start - markerLength, i*increment);
+      markers.push( line( start, i*increment, start - markerLength, i*increment ) );
     }
   }
-  return 'L' + markers.join("") + ' ';
+  return markers.join("");
 }
 
-function line( x1, x2, y1, y2 ){
-  f = f || 'transparent';
-  s = s || cutDepth;
-  sw = sw || 1;
-  // return '<line x1="' + x1 + '" x2="' + x2 + '" y1="' + y1 + '" y2="' + y2 + '" stroke="' + s + '" fill="' + f + '" stroke-width="' + sw + '" />';
-  return "M" + [x1,y1].join(',') + " L" + [x2,y2].join(',') + ' ';
+function moveTO(x,y){
+  return "M" + [x,y].join(',') + " ";
 }
 
-function rect( x, y, w, h, s, f, sw, rx, ry ){
-  f = f || 'transparent';
-  s = s || cutDepth;
-  sw = sw || 1;
-  rx = rx || 0;
-  ry = ry || 0;
-  return '<rect x="' + x + '" y="' + y + '" rx="' + rx + '" ry="' + ry + '" width="' + w + '" height="' + h + '" stroke="' + s + '" fill="' + f + '" stroke-width="' + sw + '" />';
+function line( x1, y1, x2, y2 ){
+  return moveTO(x1,y1) + "L" + [x2,y2].join(',') + ' ';
+}
+
+function rect( x, y, w, h ){
+  // top, bottom, left, right
+  return [
+    line( x, y, x+w, y ),
+    line( x, y+h, x+w, y+h ),
+    line( x, y, x, y+h ),
+    line( x+w, y, x+w, y+h )
+  ].join('');
 }
 
 
